5a4766acb3b58caa6b0dbd3a68b3adf83fb96d88
thuchagas <thuchagascorreia@gmail.com>
2023-11-02 12:55:09 +0000

    fix

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import '../components/navbar.css'
+
+
+
+function NavBar(){
+    return(
+        
+                <nav>
+                    <ul>
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/acerca-de">Acerca de</a></li>
+                        <li><a href="/contacto">Contacto</a></li>
+                    </ul>
+                </nav>
+            );
+        }
+        
+        export default NavBar;
+
